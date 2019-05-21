24f7437e42d188c6f93e92380520c33710173fed
JWolf <wmtorode@gmail.com>
2019-05-21 03:31:52 -0400

    log when a save has been edited

---
+++ Source/SaveEditLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Harmony;
+using System.Reflection;
+
+namespace JW_SaveEditLogger
+{
+    public class SaveEditLogger
+    {
+
+        public static void Init(string directory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("JWolf.SaveEditLogger");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+}
---
+++ Source/SimGameState_RehydratePatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Harmony;
+using BattleTech;
+using BattleTech.Save;
+
+namespace JW_SaveEditLogger
+{
+    [HarmonyPatch(typeof(SimGameState), "Rehydrate", typeof(GameInstanceSave))]
+    class SimGameState_RehydratePatch
+    {
+        public static void Postfix(SimGameState __instance, GameInstanceSave gameInstanceSave)
+        {
+            if (__instance.CompanyStats.ContainsStatistic("BtSaveEdit.Version"))
+            {
+                string value = __instance.CompanyStats.GetValue<string>("BtSaveEdit.Version");
+                SimGameState.logger.Log(string.Format("The save for this career/campaign has been edited by Save Editor version: {0}", (object) value));
+            }
+        }
+
+
+    }
+}
