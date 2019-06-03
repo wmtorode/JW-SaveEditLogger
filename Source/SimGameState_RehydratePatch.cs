9d5716e430c0396b5cd7191ad432e6310ce63e1f
JWolf <wmtorode@gmail.com>
2019-06-03 00:52:29 -0400

    add logging for recent save edits

--- Source/SimGameState_RehydratePatch.cs
+++ Source/SimGameState_RehydratePatch.cs
@@ -14,6 +14,23 @@ namespace JW_SaveEditLogger
             {
                 string value = __instance.CompanyStats.GetValue<string>("BtSaveEdit.Version");
                 SimGameState.logger.Log(string.Format("The save for this career/campaign has been edited by Save Editor version: {0}", (object) value));
+
+                if (__instance.CompanyStats.ContainsStatistic("BtSaveEdit.LogCount"))
+                {
+                    int logCount = __instance.CompanyStats.GetValue<int>("BtSaveEdit.LogCount");
+                    if (logCount > 0)
+                    {
+                        SimGameState.logger.Log("Most recent save edits:");
+                        for (int i = 0; i < logCount; i++)
+                        {
+                            if (__instance.CompanyStats.ContainsStatistic(string.Format("BtSaveEdit.LogItem{0}", (object) i)))
+                            {
+                                string log = __instance.CompanyStats.GetValue<string>(string.Format("BtSaveEdit.LogItem{0}", (object)i));
+                                SimGameState.logger.Log(string.Format("{0}: {1}", (object)i, (object) log));
+                            }
+                        }
+                    }
+                }
             }
         }
 
